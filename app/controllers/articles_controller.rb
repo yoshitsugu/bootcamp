f956365079ed4a2610302b00e5ffb1962c7fb16f
teppei machida <machida@fjord.jp>
2021-05-14 00:09:45 +0900

    ブログをlpのレイアウトで表示するようにした

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all.order(created_at: :desc)
     @articles = @articles.tagged_with(params[:tag]) if params[:tag]
-    render layout: 'article'
+    render layout: 'welcome'
   end
 
   def show
